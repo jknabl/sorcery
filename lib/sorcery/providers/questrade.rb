c63bd01edd4e589fbc455cd3aa4f33e8941e1e8e
Jason Knabl <jason.knabl@gmail.com>
2020-10-10 19:26:31 -0400

    The questrade API uses different base URLs for auth/token flows and fetching user info. Toggle the site URL on the OAuth client when making calls to fetch account info.

--- lib/sorcery/providers/questrade.rb
+++ lib/sorcery/providers/questrade.rb
@@ -3,25 +3,34 @@ module Sorcery
     class Questrade < Base
       include Protocols::Oauth2
 
-      attr_accessor :auth_site, :auth_path, :scope, :token_path, :user_info_path
+      attr_accessor :auth_site, :auth_path, :scope, :token_path, :user_info_path, :token, :api_url
 
       def initialize
         super
 
+        @api_url = nil
+        @token = nil
         @scope = 'read_acc read_md'
         @site = 'https://login.questrade.com/'
         @auth_path = '/oauth2/authorize'
         @token_path = "/oauth2/token"
-        @user_info_path = "/accounts"
+        @user_info_path = "/v1/accounts"
         @state = SecureRandom.hex(16)
       end
 
       def get_user_hash(access_token)
-        response = access_token.get(user_info_path, params: { token: access_token.token })
-        body = JSON.parse(response.body)
-        auth_hash(access_token).tap do |h|
-          h[:user_info] = body
-          h[:uid] = body['id']
+        begin
+          @token.client.site = @api_url
+
+          response = access_token.get(user_info_path)
+
+          body = JSON.parse(response.body)
+          auth_hash(access_token).tap do |h|
+            h[:user_info] = body
+            h[:uid] = body['userId']
+          end
+        ensure
+          @token.client.site = @site
         end
       end
 
@@ -33,13 +42,18 @@ module Sorcery
         args = {}.tap do |a|
           a[:code] = _params[:code] if _params[:code]
         end
-        get_access_token(
-          args,
+
+        args = args
+        options = {
           token_url: token_path,
           client_id: @key,
           grant_type: 'authorization_code',
-          token_method: :post
-        )
+          token_method: :post,
+        }
+        @token = get_access_token(args, options)
+        @api_url = token.params['api_server']
+
+        token
       end
     end
   end
